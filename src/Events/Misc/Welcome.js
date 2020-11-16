7bec35475605dae7d86f753baac8d42d45671dac
iiAlosha <arma.55@outlook.com>
2020-11-16 19:17:27 +0300

    Update Welcome.js

--- src/Events/Misc/Welcome.js
+++ src/Events/Misc/Welcome.js
@@ -21,23 +21,21 @@ module.exports = async (member) => {
         if(channel) channel.csend(`${member} kişisi sunucuya katıldı ve aktif bir jail kaydı bulundu ve jail'e atıldı.`);
         return;
     }
-    let giveRoles = [];
+    let giveRoles = Settings.Roles.Unregistered;
     let mute = penals.some(e => e.Type == PenalManager.Types.MUTE || e.Type == PenalManager.Types.TEMP_MUTE);
     if(mute) giveRoles.concat(Settings.Penals.Mute.Role);
     let voiceMute = penals.some(e => e.Type == PenalManager.Types.VOICE_MUTE || e.Type == PenalManager.Types.TEMP_VOICE_MUTE);
     if(voiceMute && Settings.Penals.VoiceMute.Role.length > 0) giveRoles.concat(Settings.Penals.VoiceMute.Role);
-
-    if(Settings.Roles.Unregistered.some(e => !member.roles.cache.has(e))) giveRoles.concat(Settings.Roles.Unregistered);
     if(giveRoles.length > 0) member.setRoles(giveRoles);
 
     channel.csend(`
-    <a:18:770228020883357697> ${member} sunucuya hoş geldin
+    :game_die: ${member} sunucuya hoş geldin
 
-<a:25:770227938796109824> Sol tarafta bulunan kayıt odalarından birine girerek kayıt olabilirsin.
+:heart: Sol tarafta bulunan kayıt odalarından birine girerek kayıt olabilirsin.
 
-<a:28:770227938624536586> Kayıt odasında bir yetkili yoksa @Kayıt Hammer rolündeki üyeleri etiketlemen yeterli.
+:full_moon: Kayıt odasında bir yetkili yoksa @Kayıt Hammer rolündeki üyeleri etiketlemen yeterli.
 
-<a:22:770227929854115890> Eğer ayrıcalıklı olmak ve bize destek olmak istiyorsan ailemize katılabilirsin!
+:full_moon: Eğer ayrıcalıklı olmak ve bize destek olmak istiyorsan ailemize katılabilirsin!
 👑 Her hafta turnuvalar düzenlenmektedir.`)
 }
 
