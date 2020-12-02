a16f29e565bac1a44c4cb78d36aa81197b994aac
iiAlosha <arma.55@outlook.com>
2020-12-02 19:50:59 +0300

    Some Fixs

--- src/Commands/Info/Sicil.js
+++ src/Commands/Info/Sicil.js
@@ -12,52 +12,53 @@ const Penal = require("../../Utils/Schemas/Penal");
  * @param {Array<String>} args 
  */
 module.exports.execute = async (client, message, args) => {
-    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("bunu yapmak için yeterli bir yetkiye sahip değilsin.");
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("bunu yapmak için yeterli bir yetkiye sahip değilsin.");
 
     let victim = message.mentions.users.first() || (args[0] ? await client.getUser(args[0]) : undefined);
-    if(!victim) return message.reply("birisini etiketlemelisin.")
+    if (!victim) return message.reply("birisini etiketlemelisin.")
 
-    Penal.find({ User: victim.id }, async (err ,res) => {
-        if(err) return message.reply("bazı problemler yaşıyoruz :(");
+    Penal.find({ User: victim.id }, async (err, res) => {
+        if (err) return message.reply("bazı problemler yaşıyoruz :(");
         res = res.reverse();
 
         let page = 1;
-        const liste = res.map((e, i) => `\`#${e.Id}:\` \`${e.Activity == true ? "✅" : "❌"}\` **[${e.Type}]** <@${e.Admin}>: **${e.Reason}** - ${moment(e.Date).tz("Europe/Istanbul").format("YYYY.MM.DD | HH:mm:ss")}`);
+
+        const liste = res.map((e, i) => `\`#${e.Id}:\` \`${e.Activity == true ? "✅" : "❌"}\` **[${e.Type}]** <@${e.Admin}>: **${e.Reason}** - ${moment(e.Time).tz("Europe/Istanbul").format("YYYY.MM.DD | HH:mm:ss")}`);
 
         var msg = await message.channel.send(new Discord.MessageEmbed()
-        .setDescription(`${message.guild.name} sunucunda ${victim} kullanıcısının tüm cezaları aşağıda listenmiştir. Cezaların hiçbiri silinmemektedir, aktif olmayan cezaların yanında :x: aktif olanların yanında ✅ işareti vardır.`)
-        .addField(`Cezalar`, `${liste.slice(page == 1 ? 0 : page * 10 - 10, page * 10).join("\n")} ** **`)
-        .setAuthor(message.author.username, message.author.avatarURL({dynamic: true}))).then(e => e);
-        
-        if(liste.length > 10){
+            .setDescription(`${message.guild.name} sunucunda ${victim} kullanıcısının tüm cezaları aşağıda listenmiştir. Cezaların hiçbiri silinmemektedir, aktif olmayan cezaların yanında :x: aktif olanların yanında ✅ işareti vardır.`)
+            .addField(`Cezalar`, `${liste.slice(page == 1 ? 0 : page * 10 - 10, page * 10).join("\n")} ** **`)
+            .setAuthor(message.author.username, message.author.avatarURL({ dynamic: true }))).then(e => e);
+
+        if (liste.length > 10) {
             await msg.react(`◀`);
             await msg.react(`▶`);
             await msg.react(`🔴`);
 
-            let collector = msg.createReactionCollector((react, user) => ["◀","▶", "🔴"].some(e => e == react.emoji.name) && user.id == message.member.id, {
+            let collector = msg.createReactionCollector((react, user) => ["◀", "▶", "🔴"].some(e => e == react.emoji.name) && user.id == message.member.id, {
                 time: 200000
             });
 
             collector.on("collect", (react, user) => {
-                if(react.emoji.name == "▶"){
-                    if(liste.slice((page + 1) * 10 - 10, (page + 1) * 10).length <= 0) return;
+                if (react.emoji.name == "▶") {
+                    if (liste.slice((page + 1) * 10 - 10, (page + 1) * 10).length <= 0) return;
                     page += 1;
                     let newList = liste.slice(page == 1 ? 0 : page * 10 - 10, page * 10).join("\n");
                     msg.edit(new Discord.MessageEmbed()
-                    .setDescription(`${message.guild.name} sunucunda ${victim} kullanıcısının tüm cezaları aşağıda listenmiştir. Cezaların hiçbiri silinmemektedir, aktif olmayan cezaların yanında :x: aktif olanların yanında ✔ işareti vardır.`)
-                    .addField(`Cezalar`, `${newList} ** **`)
-                    .setAuthor(message.author.username, message.author.avatarURL({dynamic: true})));
+                        .setDescription(`${message.guild.name} sunucunda ${victim} kullanıcısının tüm cezaları aşağıda listenmiştir. Cezaların hiçbiri silinmemektedir, aktif olmayan cezaların yanında :x: aktif olanların yanında ✔ işareti vardır.`)
+                        .addField(`Cezalar`, `${newList} ** **`)
+                        .setAuthor(message.author.username, message.author.avatarURL({ dynamic: true })));
                 }
-                if(react.emoji.name == "◀"){
-                    if(liste.slice((page - 1) * 10 - 10, (page - 1) * 10).length <= 0) return;
+                if (react.emoji.name == "◀") {
+                    if (liste.slice((page - 1) * 10 - 10, (page - 1) * 10).length <= 0) return;
                     page -= 1;
                     let newList = liste.slice(page == 1 ? 0 : page * 10 - 10, page * 10).join("\n");
                     msg.edit(new Discord.MessageEmbed()
-                    .setDescription(`${message.guild.name} sunucunda ${victim} kullanıcısının tüm cezaları aşağıda listenmiştir. Cezaların hiçbiri silinmemektedir, aktif olmayan cezaların yanında :x: aktif olanların yanında ✔ işareti vardır.`)
-                    .addField(`Cezalar`, `${newList} ** **`)
-                    .setAuthor(message.author.username, message.author.avatarURL({dynamic: true})));
+                        .setDescription(`${message.guild.name} sunucunda ${victim} kullanıcısının tüm cezaları aşağıda listenmiştir. Cezaların hiçbiri silinmemektedir, aktif olmayan cezaların yanında :x: aktif olanların yanında ✔ işareti vardır.`)
+                        .addField(`Cezalar`, `${newList} ** **`)
+                        .setAuthor(message.author.username, message.author.avatarURL({ dynamic: true })));
                 }
-                if(react.emoji.name == "🔴"){
+                if (react.emoji.name == "🔴") {
                     msg.delete();
                     collector.stop();
                 }
---
+++ src/Commands/Penal/UnVoiceMute.js
@@ -0,0 +1,54 @@
+const { Client, Message } = require("discord.js");
+const Settings = require("../../Configuration/Settings.json");
+
+const PenalManager = require("../../Utils/Managers/PenalManager");
+const Penal = require("../../Utils/Schemas/Penal");
+
+/**
+ * @param {Client} client 
+ * @param {Message} message 
+ * @param {Array<String>} args 
+ */
+module.exports.execute = async (client, message, args) => {
+    if (!message.member.hasPermission("ADMINISTRATOR") && !Settings.Penals.Mute.AuthRoles.some(authRole => message.member.roles.cache.has(authRole))) return message.reply("yeterli yetkin yok.");
+
+    let victim = message.mentions.users.first() || client.users.cache.get(args[0]) || await client.users.getUser(args[0]);
+    if (!victim) return message.reply(`birisini etiketlemelisin.`);
+
+    let penals = await PenalManager.getPenals({ User: victim.id, Activity: true, $or: [{ Type: PenalManager.Types.TEMP_VOICE_MUTE }, { Type: PenalManager.Types.VOICE_MUTE }] });
+    if (penals.length <= 0) return message.reply(`${victim}(${victim.username}) kişisinin hiç susturma cezası yok.`);
+
+    let member = await message.guild.getMember(victim.id);
+    if (member && member.roles.highest.position >= message.member.roles.highest.position) return message.reply("senin rolünden üstte ya da aynı roldeki birisini susturamazsın.");
+
+    let cezaNumaraları = penals.map(penal => penal.Id);
+
+    let msg = await message.reply(`${victim} kişisinin toplam **${penals.length}** adet sesli susturması var. Cezalardan hangisin kaldırmak istiyorsunuz? \`(${cezaNumaraları.map(e => "#" + e).join(", ")})\``);
+
+    let messages = await msg.channel.awaitMessages((m) => m.author.id == message.author.id && cezaNumaraları.some(cevap => m.content.toLowerCase().includes(cevap)), {
+        max: 1,
+        time: 15000
+    });
+
+    if (messages.size <= 0) {
+        return message.reply(`${member ? member.displayName : victim.username}(${victim.id}) için başlatmış olduğun ceza kaldırma işlemi cevap vermediğin için iptal ediliyor.`);
+    }
+
+    let reply = messages.first();
+    let penalId = cezaNumaraları.find(e => reply.content.includes(e));
+    if (penalId) {
+        penalId = Number(penalId);
+        await Penal.updateMany({ Id: penalId }, { $set: { Activity: false } }).exec();
+        if (member && member.roles.cache.has(Settings.Penals.Mute.Role)) member.roles.remove(Settings.Penals.Mute.Role);
+
+        message.reply(`${member ? member.displayName : victim.username}(${victim.id}) kullanıcısının \`#${penalId}\` numaralı sesli sohbet cezasını kaldırdın. ✅`);
+    }
+    else message.reply(`${member ? member.displayName : victim.username}(${victim.id}) sesli susturma cezası kaldırma işlemi iptal edildi.`);
+};
+
+module.exports.settings = {
+    Commands: ["unmute"],
+    Usage: "",
+    Description: "",
+    Activity: true
+}
