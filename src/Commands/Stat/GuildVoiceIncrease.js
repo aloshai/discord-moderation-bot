489b4edb154f5968dc0762a0bb9fd015b43cde53
iiAlosha <arma.55@outlook.com>
2021-02-16 10:41:31 +0300

    XD

---
+++ src/Commands/Stat/GuildVoiceIncrease.js
@@ -0,0 +1,66 @@
+const { Client, Message, MessageEmbed, MessageAttachment } = require("discord.js");
+const Guild = require("../../Models/Database/Guild");
+
+const moment = require("moment");
+require("moment-duration-format");
+
+const tm = require("../../Managers/TimeManager");
+
+const cm = require("../../Managers/ChartManager");
+
+
+/**
+ * @param {Client} client 
+ * @param {Message} message 
+ * @param {Array<String>} args 
+ */
+module.exports.execute = async (client, message, args) => {
+    let data = await Guild.findOne({ Id: message.guild.id }, { "Stats.VoiceIncrease": 1 });
+    if(data.Stats.VoiceIncrease){
+        let day = await tm.getDay(message.guild.id);
+        let chartDates = [], chartValues = new Array(day).fill(0);
+
+        for (let index = 0; index < day; index++) chartDates.push(new Date(Date.now() - (1000 * 60 * 60 * 24 * (day - (index + 1)))).toTurkishFormatDate());
+
+        let voiceData = data.Stats.VoiceIncrease;
+        let days = Object.keys(voiceData);
+        days.forEach(_day => {
+            let sum = Object.values(voiceData[_day]).reduce((x, y) => x + y, 0);
+            chartValues[_day - 1] = sum;
+        });
+
+        let buffer = await cm.ImageFromData({
+            width: 600,
+            height: 290,
+            type: 'line',
+    
+            data: {
+                labels: [].concat(chartDates),
+                datasets: [{
+                    label: "Toplam Mesaj İstatistiği (adet)",
+                    data: [].concat(chartValues),
+                    backgroundColor: [
+                        'rgba(0, 112, 255, 0.25)'
+                    ],
+                    borderColor: [].concat(new Array(day).fill('rgba(0, 92, 210, 0.5)')),
+                    borderWidth: 1
+                }]
+            }
+        });
+    
+        let attachment = new MessageAttachment(buffer, "Graph.png");
+        message.channel.csend({
+            files: [attachment]
+        });
+    }
+    else message.reply("kayıtlı hiçbir veri bulunmamaktadır.");
+}
+
+module.exports.settings = {
+    Commands: ["gvi", "guildvoiceincrease"],
+    Usage: "gvi",
+    Description: "Sunucudaki aktifliğin nasıl sürdürüldüğüne bakarsın.",
+    Category: "Stats",
+    Activity: true,
+    cooldown: 15000
+}
--- src/Models/Database/Guild.js
+++ src/Models/Database/Guild.js
@@ -4,7 +4,8 @@ const schema = new mongoose.Schema({
   Id: String,
   Day: { type: Number, default: 1 },
   NextUpdate: { type: Number, default: new Date().setHours(24, 0, 0, 0) },
-  AuthRanks: { type: Array, default: [] }
+  AuthRanks: { type: Array, default: [] },
+
 });
 
 const model = mongoose.model("Guilds", schema);
