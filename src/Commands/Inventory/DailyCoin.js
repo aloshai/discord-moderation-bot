2ba969b9e5e413f6cfe7c4c7c6b6f7b097ca4c0b
Mert Yılmaz <arma.55@outlook.com>
2021-04-21 07:12:46 +0300

    Update DailyCoin.js

--- src/Commands/Inventory/DailyCoin.js
+++ src/Commands/Inventory/DailyCoin.js
@@ -15,20 +15,20 @@ require("moment-timezone");
 module.exports.execute = async (client, message, args) => {
     let user = await User.findOrCreate(message.author.id);
 
-    if(user.DailyCoin){
-        let nextDay = user.DailyCoin + (1000 * 60 * 60 * 24 * 1);
-        if(Date.now() < nextDay){
+    if (user.DailyCoin) {
+        let nextDay = user.DailyCoin + (1000 * 60 * 60 * 2);
+        if (Date.now() < nextDay) {
             return message.reply(`yeni puan almak için **${moment.duration((nextDay - Date.now())).format("HH:mm:ss")} saat** beklemen gerekiyor!`);
         }
     }
-    User.updateOne({_id: user._id}, {$set: {"DailyCoin": Date.now()}, $inc: {"Coin": 50}}).exec();
+    User.updateOne({ _id: user._id }, { $set: { "DailyCoin": Date.now() }, $inc: { "Coin": 50 } }).exec();
     message.reply(`günlük :coin:**${InventoryManager.Number(50)}** aldın!`);
 }
 
 module.exports.settings = {
     Commands: ["dailycoin"],
     Usage: "dailycoin",
-    Description: "Günlük alabileceğin puanı alırsın.",    
+    Description: "Günlük alabileceğin puanı alırsın.",
     Category: "Economy",
     cooldown: 5000,
     Activity: true
