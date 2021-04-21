fbfbd5a6ade87fb324dac8d75b1057ba1bdad6aa
Mert Yılmaz <arma.55@outlook.com>
2021-04-21 07:13:12 +0300

    Update DailyCrate.js

--- src/Commands/Inventory/DailyCrate.js
+++ src/Commands/Inventory/DailyCrate.js
@@ -15,15 +15,15 @@ require("moment-timezone");
 module.exports.execute = async (client, message, args) => {
     let user = await User.findOrCreate(message.author.id);
 
-    if(user.DailyCrate){
-        let nextDay = user.DailyCrate + (1000 * 60 * 60 * 24 * 1);
-        if(Date.now() < nextDay){
+    if (user.DailyCrate) {
+        let nextDay = user.DailyCrate + (1000 * 60 * 60 * 2);
+        if (Date.now() < nextDay) {
             return message.reply(`yeni kasa almak için **${moment.duration((nextDay - Date.now())).format("HH:mm:ss")} saat** beklemen gerekiyor!`);
         }
     }
     let item = InventoryManager.FindItem("COIN_CRATE");
     InventoryManager.addItemOfInventory(user, item, 1);
-    User.updateOne({_id: user._id}, {$set: {"DailyCrate": Date.now()}}).exec();
+    User.updateOne({ _id: user._id }, { $set: { "DailyCrate": Date.now() } }).exec();
     message.reply(`günlük ${message.guild.findEmoji(item.Symbol)}**${InventoryManager.Number(1)}** aldın!`);
 }
 
